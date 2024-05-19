6d6901aac856db7837b0cba1de761c231148d716
Jacobgarm <jacobgarm@hotmail.dk>
2024-05-20 00:17:59 +0200

    Inlay hints

--- init.lua
+++ init.lua
@@ -911,6 +911,7 @@ require('lazy').setup({
 })
 -- Custom bits
 vim.g.instant_username = 'jacob'
+vim.lsp.inlay_hint.enable(true)
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
